14693c612881a2565be3d7374f2bc6f83f37c9ec
Sabin Badal <55592534+BadalSabin@users.noreply.github.com>
2022-11-08 10:31:45 +0545

    Implemented Prim MST Algo without animation

---
+++ AlgoViz/src/app/graph/PrimsMST.cpp
@@ -0,0 +1,79 @@
+#include "PrimsMST.h"
+
+PrimsMST::PrimsMST()
+{
+}
+
+PrimsMST::PrimsMST(QGraphicsScene* graphicsScene, QPlainTextEdit* primsMSTConsole)
+	:graphicsScene(graphicsScene), primsMSTConsole(primsMSTConsole)
+{
+	primsMSTGraph = new Graph(graphicsScene, true);
+}
+
+PrimsMST::~PrimsMST()
+{
+}
+
+void PrimsMST::run(int startNodeId)
+{
+
+	std::vector<Node*>* nodesInGraph = primsMSTGraph->getNodesInGraph();
+	std::map<int, std::vector<int>*>* nodeNeighbours = primsMSTGraph->getNodeNeighbours();
+	std::vector<std::vector<int>*>* weightMatrix = primsMSTGraph->getWeightMatrix();
+
+	std::vector<int>* parent = new std::vector<int>(nodesInGraph->size(), 0);
+	std::vector<int>* key = new std::vector<int>(nodesInGraph->size(), std::numeric_limits<int>::max());
+	std::vector<bool>* mstSet = new std::vector<bool>(nodesInGraph->size(), false);
+
+	key->at(0) = 0;
+	parent->at(startNodeId) = -1;
+
+	for (int i = 0; i < nodesInGraph->size() - 1; i++)
+	{
+		int nextNodeWithMinKey = minKey(key, mstSet);
+		mstSet->at(nextNodeWithMinKey) = true;
+
+		for (int v = 0; v < nodesInGraph->size(); v++)
+		{
+			if (weightMatrix->at(nextNodeWithMinKey)->at(v) && mstSet->at(v) == false
+				&& weightMatrix->at(nextNodeWithMinKey)->at(v) < key->at(v))
+			{
+				parent->at(v) = nextNodeWithMinKey;
+				key->at(v) = weightMatrix->at(nextNodeWithMinKey)->at(v);
+			}
+		}
+	}
+	printMST(parent, weightMatrix);
+}
+
+void PrimsMST::displayGraph()
+{
+	primsMSTGraph->displayGraphNodes();
+}
+
+int PrimsMST::minKey(std::vector<int>* key, std::vector<bool>* mstSet)
+{
+	int min = std::numeric_limits<int>::max();
+	int minIndex = -1;
+
+	for (int i = 0; i < primsMSTGraph->getNodesInGraph()->size(); i++)
+	{
+		if (mstSet->at(i) == false && key->at(i) < min)  {
+			min = key->at(i);
+			minIndex = i;
+		}
+	}
+	return minIndex;
+}
+
+void PrimsMST::printMST(std::vector<int>* parent, std::vector<std::vector<int>*>* weightMatrix)
+{
+	QString soln = "Edge \tWeight\n";
+	int parentNodeId = 0;
+	for (int i = 1; i < primsMSTGraph->getNodesInGraph()->size(); i++)
+	{
+		parentNodeId = parent->at(i);
+		soln.append(QString::number(parentNodeId) + " - " + QString::number(i) + " \t" + QString::number(weightMatrix->at(parentNodeId)->at(i)) + "\n");
+		primsMSTConsole->setPlainText(soln);
+	}
+}
---
+++ AlgoViz/src/app/graph/PrimsMST.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Graph.h"
+#include <queue>
+#include <vector>
+#include <QPlainTextEdit>
+#include <QLabel>
+#include "../animation/PauseAnim.h"
+#include <limits>
+#include <QTextCodec>
+#include <qtextcodec>
+class PrimsMST
+{
+	Graph* primsMSTGraph;
+	QGraphicsScene* graphicsScene;
+	QPlainTextEdit* primsMSTConsole;
+public:
+	PrimsMST();
+	PrimsMST(QGraphicsScene* graphicsScene, QPlainTextEdit* primsMSTConsole);
+	~PrimsMST();
+
+	void run(int startNodeId);
+	void displayGraph();
+
+private:
+	int minKey(std::vector<int>* key, std::vector<bool>* mstSet);
+	void printMST(std::vector<int>* parent, std::vector<std::vector<int>*>* weightMatrix);
+};
+
