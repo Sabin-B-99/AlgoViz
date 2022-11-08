400912d3ab60ea21819e16e6b03b472351891438
Sabin Badal <55592534+BadalSabin@users.noreply.github.com>
2022-11-08 21:02:19 +0545

    Implemented Kruskal's MST algorithm with animation

--- AlgoViz/src/app/graph/KruskalMST.cpp
+++ AlgoViz/src/app/graph/KruskalMST.cpp
@@ -30,7 +30,10 @@ void KruskalMST::run()
 	int ans = 0;
 	kruskalMSTConsole->setPlainText(soln);
 
-
+	Node* currentNode = nullptr;
+	Node* nextNode = nullptr;
+	Line* nodeConnectingLine = nullptr;
+	std::vector<std::pair<int, int>*>* mstSolnEdges = new std::vector<std::pair<int, int>*>;
 
 	for (std::vector<std::vector<int>>::iterator it = edgeList.begin(); it != edgeList.end(); it++)
 	{
@@ -38,13 +41,39 @@ void KruskalMST::run()
 		int y = (*it).at(1);
 		int w = (*it).at(2);
 
+		currentNode = nodesInGraph->at(x);
+		nextNode = nodesInGraph->at(y);
+		nodeConnectingLine = kruskalMSTGraph->getConnectingLine(currentNode, nextNode);
+		if (nodeConnectingLine) {
+			currentNode->setNodeOutlinePen(new QPen(Qt::blue));
+			nextNode->setNodeOutlinePen(new QPen(Qt::red));
+			nodeConnectingLine->setLineStrokePen(new QPen(Qt::red));
+			graphicsScene->update();
+			PauseAnim::showPauseAnimation(1200);
+			currentNode->setNodeOutlinePen(new QPen(Qt::black));
+			nextNode->setNodeOutlinePen(new QPen(Qt::black));
+			nodeConnectingLine->setLineStrokePen(new QPen(Qt::black));
+			graphicsScene->update();
+		}
+
 		if (s->find(x) != s->find(y)) {
 			s->unite(x, y);
 			ans += w;
-			soln.append(QString::number(x) + " -- " + QString::number(y) + " == " + QString::number(w) + "\n");
+
+			soln.append(QString::number(x) + " -- " + QString::number(y) + " == " + QString::number(w) +  "\n");
 			kruskalMSTConsole->setPlainText(soln);
+			mstSolnEdges->emplace_back(new std::pair<int, int>(x,y));
 		}
 	}
+
+	for (std::vector<std::pair<int,int>*>::iterator it = mstSolnEdges->begin(); it != mstSolnEdges->end(); it++)
+	{
+		currentNode = nodesInGraph->at((**it).first);
+		nextNode = nodesInGraph->at((**it).second);
+		nodeConnectingLine = kruskalMSTGraph->getConnectingLine(currentNode, nextNode);
+		nodeConnectingLine->setLineStrokePen(new QPen(Qt::red, 2));
+	}
+
 }
 
 void KruskalMST::displayGraph()
