10c4469cbc0d8eb54ed4c65a2b6cae93b0fd596b
Sabin Badal <55592534+BadalSabin@users.noreply.github.com>
2022-11-12 17:21:13 +0545

    Implemented buttons to visualize different algorithms

--- AlgoViz/src/app/AlgoViz.cpp
+++ AlgoViz/src/app/AlgoViz.cpp
@@ -6,10 +6,29 @@ AlgoViz::AlgoViz(QWidget *parent)
 {  
     ui.setupUi(this);
     ui.stackedWidget->addWidget(&bst);
+    ui.stackedWidget->addWidget(&bfs);
+    ui.stackedWidget->addWidget(&dfs);
+    ui.stackedWidget->addWidget(&dijSPF);
+    ui.stackedWidget->addWidget(&primMST);
+    ui.stackedWidget->addWidget(&kruskalMST);
+    ui.stackedWidget->addWidget(&seqSrch);
+    ui.stackedWidget->addWidget(&binSrch);
+    ui.stackedWidget->addWidget(&stack);
+    ui.stackedWidget->addWidget(&q);
 
     ui.stackedWidget->setCurrentIndex(0);
 
     connect(&bst, SIGNAL(homeClicked()), this, SLOT(moveHome()));
+
+    connect(&bfs, SIGNAL(homeClicked()), this, SLOT(moveHome()));
+    connect(&dfs, SIGNAL(homeClicked()), this, SLOT(moveHome()));
+    connect(&dijSPF, SIGNAL(homeClicked()), this, SLOT(moveHome()));
+    connect(&primMST, SIGNAL(homeClicked()), this, SLOT(moveHome()));
+    connect(&kruskalMST, SIGNAL(homeClicked()), this, SLOT(moveHome()));
+    connect(&seqSrch, SIGNAL(homeClicked()), this, SLOT(moveHome()));
+    connect(&binSrch, SIGNAL(homeClicked()), this, SLOT(moveHome()));
+    connect(&stack, SIGNAL(homeClicked()), this, SLOT(moveHome()));
+    connect(&q, SIGNAL(homeClicked()), this, SLOT(moveHome()));
 }
 
 AlgoViz::~AlgoViz()
@@ -23,3 +42,51 @@ void AlgoViz::moveHome()
 void AlgoViz::on_bstButton_clicked() {
     ui.stackedWidget->setCurrentIndex(1);
 }
+
+void AlgoViz::on_bfsButton_clicked()
+{
+    ui.stackedWidget->setCurrentIndex(2);
+}
+
+void AlgoViz::on_dfsButton_clicked()
+{
+    ui.stackedWidget->setCurrentIndex(3);
+}
+
+void AlgoViz::on_dijkstraButton_clicked()
+{
+    ui.stackedWidget->setCurrentIndex(4);
+}
+
+void AlgoViz::on_primButton_clicked()
+{
+    ui.stackedWidget->setCurrentIndex(5);
+}
+
+void AlgoViz::on_kruskalButton_clicked()
+{
+    ui.stackedWidget->setCurrentIndex(6);
+}
+
+void AlgoViz::on_seqSrchButton_clicked()
+{
+    ui.stackedWidget->setCurrentIndex(7);
+}
+
+void AlgoViz::on_binSrchButton_clicked()
+{
+    ui.stackedWidget->setCurrentIndex(8);
+}
+
+void AlgoViz::on_stackButton_clicked()
+{
+    ui.stackedWidget->setCurrentIndex(9);
+}
+
+void AlgoViz::on_queueButton_clicked()
+{
+    ui.stackedWidget->setCurrentIndex(10);
+}
+
+
+
--- AlgoViz/src/app/AlgoViz.h
+++ AlgoViz/src/app/AlgoViz.h
@@ -2,7 +2,16 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_AlgoViz.h"
-#include "tree/BSTView.h"
+#include "tree/ui_classes/BSTView.h"
+#include "graph/ui_classes/BFSView.h"
+#include "graph/ui_classes/DFSView.h"
+#include "graph/ui_classes/DijkstraSPFView.h"
+#include "graph/ui_classes/KruskalMSTView.h"
+#include "graph/ui_classes/PrimsMSTView.h"
+#include "linear_structure/ui_classes/StackView.h"
+#include "linear_structure/ui_classes/QueueView.h"
+#include "search/ui_classes/SeqSrchView.h"
+#include "search/ui_classes/BinSrchView.h"
 
 class AlgoViz : public QMainWindow
 {
@@ -14,9 +23,28 @@ public:
 
 private slots:
     void on_bstButton_clicked();
+    void on_bfsButton_clicked();
+    void on_dfsButton_clicked();
+    void on_dijkstraButton_clicked();
+    void on_primButton_clicked();
+    void on_kruskalButton_clicked();
+    void on_seqSrchButton_clicked();
+    void on_binSrchButton_clicked();
+    void on_stackButton_clicked();
+    void on_queueButton_clicked();
     void moveHome();
 
 private:
     Ui::AlgoVizClass ui;
     BSTView bst;
+
+    BFSView bfs;
+    DFSView dfs;
+    DijkstraSPFView dijSPF;
+    PrimsMSTView primMST;
+    KruskalMSTView kruskalMST;
+    StackView stack;
+    QueueView q;
+    SeqSrchView seqSrch;
+    BinSrchView binSrch;
 };
