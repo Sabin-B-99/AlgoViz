555cd24dbf1699ef3b77f7b38dff85feecf24c82
Sabin Badal <55592534+BadalSabin@users.noreply.github.com>
2022-11-21 13:45:34 +0545

    Removed the starting node selection line edit

--- AlgoViz/src/app/graph/ui_classes/PrimsMSTView.cpp
+++ AlgoViz/src/app/graph/ui_classes/PrimsMSTView.cpp
@@ -8,14 +8,7 @@ PrimsMSTView::PrimsMSTView(QWidget* parent)
 	ui.primAnimArea->setScene(scene);
 	ui.primAnimArea->setRenderHint(QPainter::Antialiasing);
 	ui.primAnimArea->setAlignment(Qt::AlignTop);
-	ui.primInsertLineEdit->setPlaceholderText("Starting Node");
 
-	intValidator = new QIntValidator();
-	intValidator->setRange(-9999, 9999);
-	regExp = new QRegularExpression();
-	regExp->setPattern("^$");
-	regExpValidator = new QRegularExpressionValidator(*regExp);
-	ui.primInsertLineEdit->setValidator(intValidator);
 
 	primMST = new PrimsMST(scene, ui.primPlainTextEdit);
 	primMST->displayGraph();
@@ -35,12 +28,7 @@ void PrimsMSTView::on_primHomeBtn_clicked() {
 
 void PrimsMSTView::on_primInsertBtn_clicked()
 {
-	QString insertVal = ui.primInsertLineEdit->text();
-	int pos = 0;
-	if (regExpValidator->validate(insertVal, pos) == QValidator::State::Acceptable) {
-		insertVal = "0";
-	}
-	primMST->run(insertVal.toInt());
+	primMST->run();
 }
 
 void PrimsMSTView::on_primNewGraphBtn_clicked()
